ea8417e7706cf3b41641b4ef9912d16c61ba9609
David Naußed <David.Naussed.5395@student.uu.se>
2019-01-26 17:02:05 +0100

    Attack animation holder and collision

---
+++ GGJ2019/Assets/Scripts/BasicAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttack : MonoBehaviour
+{
+    public GameObject hitbox;
+    public float cooldown = 4;
+    private float collisionBuffer = 0;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (cooldown <= 0 && Input.GetKey("f"))
+        {
+            hitbox.SetActive(true);
+            collisionBuffer = 0.5f;
+            cooldown = 4;
+        }
+        else if (collisionBuffer <= 0)
+            hitbox.SetActive(false);
+
+        if(cooldown != 0)
+            if (cooldown >= 0)
+            cooldown -= Time.deltaTime;
+            else if (cooldown <= 0)
+            cooldown = 0;
+        collisionBuffer-= Time.deltaTime;
+    }
+}
